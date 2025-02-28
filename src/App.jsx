af7b9f059ac2cca20ade884dbe2479c4863b9347
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-02-28 23:03:58 +0545

    Worling on navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
-function App() {
-  return <p className="text-4xl">HELLO WORLD</p>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from "./components";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="realtive z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
