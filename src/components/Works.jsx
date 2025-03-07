0036d8fb8bd579b1614c8b60aa655aa298e84457
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-03-07 23:16:26 +0545

    Remove unwanted imports

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { Tilt } from "react-tilt";
 import { styles } from "../styles";
 import { github, livedemo } from "../assets";
 import { SectionWrapper } from "../HOC";
