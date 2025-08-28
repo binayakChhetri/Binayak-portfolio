d5663c9890a696dd01e3ca3e5ffc6f09c341531a
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-08-28 22:03:58 +0545

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    // <section className={`bg-hero relative w-full h-screen mx-auto`}>
+    <section className={`bg-hero relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
